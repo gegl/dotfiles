517cb5b28e877860958169f3f52b625b4a098da6
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2017-02-16 13:49:42 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Add symlinks
+ln -sfv "$DOTFILES_DIR/vimrc" ~/.vimrc
+ln -sfv "$DOTFILES_DIR/gitconfig" ~/.gitconfig
+ln -sfv "$DOTFILES_DIR/gitignore" ~/.gitignore
