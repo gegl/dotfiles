64d3d8d01c4795993125ce48270452a93e148f27
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2022-12-31 02:45:40 +0100

    Add completion plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -155,6 +155,23 @@ function M.setup()
       requires = { 'kyazdani42/nvim-web-devicons' },
     }
 
+    -- Completion
+    use {
+      "ms-jpq/coq_nvim",
+      branch = "coq",
+      event = "InsertEnter",
+      opt = true,
+      run = ":COQdeps",
+      config = function()
+        require("config.coq").setup()
+      end,
+      requires = {
+        { "ms-jpq/coq.artifacts", branch = "artifacts" },
+        { "ms-jpq/coq.thirdparty", branch = "3p", module = "coq_3p" },
+      },
+      disable = false,
+    }
+
     -- Bootstrap Neovim
     if packer_bootstrap then
       print "Restart Neovim required after installation!"
