a87b63929171ac741f585680ac536776d665016f
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2023-01-02 22:01:24 +0100

    Add doc

--- nvim/lua/config/test.lua
+++ nvim/lua/config/test.lua
@@ -7,6 +7,8 @@ function M.setup()
         let test#strategy = "neovim"
         let test#neovim#term_position = "belowright"
         let g:test#preserve_screen = 1
+
+        " Run all Ruby/rspec tests using docker-compose assuming existence of app service with Ruby image
         let test#ruby#rspec#executable = "docker-compose run --no-deps --rm -v ${PWD}:/app -w /app app bundle exec rspec"
     ]],
     false
