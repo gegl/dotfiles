ed8ffda074a11aa4aab79d46362ce0ebab22fe0b
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2023-01-02 21:49:52 +0100

    Configure run Ruby tests within Docker

--- nvim/lua/config/test.lua
+++ nvim/lua/config/test.lua
@@ -7,6 +7,7 @@ function M.setup()
         let test#strategy = "neovim"
         let test#neovim#term_position = "belowright"
         let g:test#preserve_screen = 1
+        let test#ruby#rspec#executable = "docker-compose run --no-deps --rm -v ${PWD}:/app -w /app app bundle exec rspec"
     ]],
     false
   )
