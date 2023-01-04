204629a167fd81bde87f6456fce5f98c57e05ab6
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2023-01-04 17:33:40 +0100

    Add Lua language server

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -2,7 +2,11 @@ local M = {}
 
 function M.setup()
   require("mason").setup()
-  require("mason-lspconfig").setup()
+  require("mason-lspconfig").setup {
+    automatic_installation = true,
+  }
+
+  require("lspconfig").sumneko_lua.setup {}
 end
 
 return M
