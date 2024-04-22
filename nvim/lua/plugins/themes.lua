e8e59f915dd569a49cbde18b57659dd5c2a57a38
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-04-23 00:44:16 +0200

    Add Neovim theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,7 +1,15 @@
 return {
-  "sainnhe/everforest",
-  config = function()
-    vim.cmd "colorscheme everforest"
-  end,
+  {
+    "sainnhe/everforest",
+    config = function()
+      vim.cmd "colorscheme everforest"
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
 }
 
