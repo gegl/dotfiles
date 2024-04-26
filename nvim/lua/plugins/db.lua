85b7cf40421a78edb4445367e5e00a5b35b7388b
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-04-26 18:49:53 +0200

    Change keymappings for vim-dadbod-ui

--- nvim/lua/plugins/db.lua
+++ nvim/lua/plugins/db.lua
@@ -1,18 +1,23 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
+    { 'tpope/vim-dadbod' },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'plsql' } },
   },
   init = function()
     vim.g.db_ui_save_location = '~/.config/db_ui'
     vim.g.db_ui_execute_on_save = false
     vim.g.db_ui_use_nerd_fonts = 1
+
+    local wk = require("which-key")
+    wk.register({
+      d = {
+        name = "DB",
+        u = { "<cmd>DBUIToggle<cr>", "Toggle DB UI" },
+        e = { "<Plug>(DBUI_ExecuteQuery)", "Execute SQL" },
+        s = { "<Plug>(DBUI_SaveQuery)", "Save SQL" },
+        b = { "<Plug>(DBUI_EditBindParameters)", "Edit SQL parameters" },
+      },
+    }, { prefix = "<leader>" })
   end,
 }
