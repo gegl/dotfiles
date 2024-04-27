c32cb57fe3fa131fa72617cf4799c4585ab67c28
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-04-27 23:50:22 +0200

    Add NeoAI plugin

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,10 +1,12 @@
 return {
   "Exafunction/codeium.nvim",
+  enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
   },
   config = function()
-    require("codeium").setup({})
+    require("codeium").setup({
+    })
   end
 }
