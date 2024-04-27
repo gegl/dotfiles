716d53d08442f4e3c85b9c05835a0fc11df63ad3
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-04-27 23:50:37 +0200

    Add NeoAI plugin

---
+++ nvim/lua/plugins/neoai.lua
@@ -0,0 +1,39 @@
+return {
+    "Bryley/neoai.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+        "NeoAI",
+        "NeoAIOpen",
+        "NeoAIClose",
+        "NeoAIToggle",
+        "NeoAIContext",
+        "NeoAIContextOpen",
+        "NeoAIContextClose",
+        "NeoAIInject",
+        "NeoAIInjectCode",
+        "NeoAIInjectContext",
+        "NeoAIInjectContextCode",
+    },
+    keys = function()
+      local wk = require("which-key")
+      wk.register({
+        a = {
+          name = "neoai",
+          c = { "<cmd>NeoAIToggle<CR>", "NeoAI [c]hat" },
+        },
+      }, { prefix = "<leader>" })
+    end,
+    config = function()
+        require("neoai").setup({
+          models = {
+            {
+              name = "openai",
+              model = "gpt-4-turbo",
+              params = nil,
+            },
+          },
+        })
+    end,
+}
