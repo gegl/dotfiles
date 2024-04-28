c890869e5b563a9df972b656aff50406f4602795
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-04-28 19:33:23 +0200

    Use tokyonight theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,15 +1,15 @@
 return {
   {
     "sainnhe/everforest",
-    config = function()
-      vim.cmd "colorscheme everforest"
-    end
   },
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
+    config = function()
+      vim.cmd "colorscheme tokyonight"
+    end
   }
 }
 
