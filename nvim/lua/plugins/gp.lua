b132de3569ea387a17ff369aa06481d31dc7d220
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-04-28 21:12:48 +0200

    Add example extension to explain code for gp.nvim plugin

--- nvim/lua/plugins/gp.lua
+++ nvim/lua/plugins/gp.lua
@@ -1,6 +1,17 @@
 return {
   "robitx/gp.nvim",
   config = function()
-    require("gp").setup()
+    require("gp").setup({
+      hooks = {
+        -- example of adding command which explains the selected code
+        Explain = function(gp, params)
+          local template = "I have the following code from {{filename}}:\n\n"
+          .. "```{{filetype}}\n{{selection}}\n```\n\n"
+          .. "Please respond by explaining the code above."
+          local agent = gp.get_chat_agent()
+          gp.Prompt(params, gp.Target.popup, nil, agent.model, template, agent.system_prompt)
+        end,
+      }
+    })
   end
 }
