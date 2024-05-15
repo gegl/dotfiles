31302b6ef581e113530388c9312aa35485680c2d
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-05-15 16:03:58 +0200

    Change default Neovim theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,15 +1,18 @@
 return {
   {
     "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd "colorscheme everforest"
+    end
   },
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-    config = function()
-      vim.cmd "colorscheme tokyonight"
-    end
   }
 }
 
