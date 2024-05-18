42af78200c1ca0f377c97d684e7d6c294c5ec6f6
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-05-18 16:45:26 +0200

    Configure key mappings for Copilot chat

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -6,10 +6,23 @@ return {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
-    opts = {
-      debug = false, 
-      -- See Configuration section for rest
-    },
+    config = function()
+      require("CopilotChat").setup({
+        -- Configuration options for copilot chat
+      })
+
+      local wk = require("which-key")
+      wk.register({
+        c = {
+          name = "chat",
+          c = { "<cmd>CopilotChatToggle<CR>", "toggle" },
+          e = { "<cmd>CopilotChatExplain<CR>", "[e]xplain", mode = { "n", "v" } },
+          o = { "<cmd>CopilotChatOptimize<CR>", "[o]ptimize", mode = { "n", "v" } },
+          v = { "<cmd>CopilotChatReview<CR>", "re[v]iew", mode = { "n", "v" } },
+          r = { "<cmd>CopilotChatReset<CR>", "[r]eset", mode = { "n", "v" } },
+        },
+      }, { prefix = "<leader>" })
+    end,
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
