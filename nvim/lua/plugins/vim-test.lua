f14052529b8d06e68fbe4f0ee0dd4d95c5e3ff00
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-11-04 13:53:33 +0100

    docker-compose => docker compose

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -6,10 +6,10 @@ return {
     vim.g["test#neovim#term_position"] = "belowright"
     vim.g["test#neovim#preserve_screen"] = 1
     vim.g["test#python#runner"] = "pyunit" -- pytest
-    -- Run all Ruby/rspec tests using docker-compose assuming existence of app service with Ruby image
-    vim.g["test#ruby#rspec#executable"] = "docker-compose run --no-deps --rm -v ${PWD}:/app -w /app app bundle exec rspec"
-    -- Run all JavaScript/mocha tests using docker-cmpose assuming existence of app service with Node image
-    vim.g["test#javascript#mocha#executable"] = "docker-compose run --no-deps --rm -v ${PWD}:/app -w /app app node node_modules/.bin/mocha"
+    -- Run all Ruby/rspec tests using docker compose assuming existence of app service with Ruby image
+    vim.g["test#ruby#rspec#executable"] = "docker compose run --no-deps --rm -v ${PWD}:/app -w /app app bundle exec rspec"
+    -- Run all JavaScript/mocha tests using docker compose assuming existence of app service with Node image
+    vim.g["test#javascript#mocha#executable"] = "docker compose run --no-deps --rm -v ${PWD}:/app -w /app app node node_modules/.bin/mocha"
 
     local wk = require("which-key")
     wk.register({
--- zsh/config.d/docker.zsh
+++ zsh/config.d/docker.zsh
@@ -1,9 +1,9 @@
 # Docker aliases
 alias dkp='docker ps'
-alias dku='docker-compose up -d'
-alias dkr='docker-compose run --service-ports --no-deps --rm app'
+alias dku='docker compose up -d'
+alias dkr='docker compose run --service-ports --no-deps --rm app'
 alias dkc='docker container stop $(docker container ls -a -q) && docker system prune -a -f --volumes'
 alias dks='docker container stop $(docker ps -q)'
-alias dke='docker-compose exec'
-alias dkl='docker-compose logs -f'
-alias dklr="docker-compose build $1 && docker-compose up -d --no-deps $1"
+alias dke='docker compose exec'
+alias dkl='docker compose logs -f'
+alias dklr="docker compose build $1 && docker-compose up -d --no-deps $1"
