12ba37e0673ea87f0ca19464c869b549c742ca44
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-12-03 20:09:25 +0100

    Add krew pkg manager

--- zsh/config.d/k8s.zsh
+++ zsh/config.d/k8s.zsh
@@ -6,3 +6,5 @@ alias k='kubectl'
 # Easily access psql on Kubernetes cluster. Usage: kpsql <database name>
 kpsql-run() { kubectl run -it --rm psql --image=postgres --restart=Never -- /bin/bash -c "psql postgres://$1:$1@pg:25060/$1"; }
 kpsql-exec() { kubectl exec -it psql  -- /bin/bash -c "psql postgres://$1:$1@pg:25060/$1"; }
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
