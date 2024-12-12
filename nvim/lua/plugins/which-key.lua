e1855b49aa1eb10440d2ab377d9ba4ca0e0f44a4
Geir Gluckstad <geir.gluckstad@fitnessgroup.no>
2024-12-12 10:53:19 +0100

    Turn off which-key warnings at startup

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -8,6 +8,6 @@ return {
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    notify = false,        -- Suppress Which-Key startup messages
   }
 }
